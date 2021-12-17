1631ff4f15a42e1ce2338cc3c33a612cb912ab1c
Krzysztof Srokowski <ksrokowski@MacBook-Pro-Krzysztof.local>
2021-12-18 00:55:58 +0100

    Main route modified

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const {TodoRecord} = require("../records/todo.record");
 
 const mainRouter = express.Router();
 
 mainRouter
-    .get('/', (req,res) => {
-        res.render('tasks/list-all');
+    .get('/', async (req,res) => {
+        res.render('tasks/list-all', {
+            tasks:  await TodoRecord.findAll()
+        });
     })
 
 module.exports = {
